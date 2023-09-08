b15d4d6980e787ab5f3405ca8cb17a9c92a4aa47
Achilles Benetopoulos <achilles@apple.com>
2023-09-07 21:38:47 -0700

    Add JDK21

--- src/java/org/apache/cassandra/utils/btree/BTreeSet.java
+++ src/java/org/apache/cassandra/utils/btree/BTreeSet.java
@@ -312,6 +312,48 @@ public class BTreeSet<V> implements NavigableSet<V>, List<V>
         throw new UnsupportedOperationException();
     }
 
+    @Override
+    public BTreeSet<V> reversed()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V removeLast()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V removeFirst()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V getLast()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V getFirst()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addLast(V v)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addFirst(V v)
+    {
+        throw new UnsupportedOperationException();
+    }
+
     public static class BTreeRange<V> extends BTreeSet<V>
     {
         // both inclusive
